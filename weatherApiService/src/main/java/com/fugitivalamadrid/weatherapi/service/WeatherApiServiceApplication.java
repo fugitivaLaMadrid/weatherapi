ad0c0e25321b97f5cd15f341da0d23ed454661fe
fugitiva <saralamadrid@outlook.com>
2026-06-23 19:24:39 +0200

    feat: add Lombok dependency and annotation processing
    - Add Lombok 1.18.30 to weatherApiCommon and weatherApiService modules
    - Configure Maven annotation processor for Lombok compilation
    - Apply @Data, @NoArgsConstructor, @AllArgsConstructor annotations to Location

--- weatherApiService/src/main/java/com/fugitivalamadrid/weatherapi/service/WeatherApiServiceApplication.java
+++ weatherApiService/src/main/java/com/fugitivalamadrid/weatherapi/service/WeatherApiServiceApplication.java
@@ -2,12 +2,17 @@ package com.fugitivalamadrid.weatherapi.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Weather API Service main application class
  * Handles weather service operations with PostgreSQL backend
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.fugitivalamadrid.weatherapi.service",
+    "com.fugitivalamadrid.weatherapi.common"
+})
 public class WeatherApiServiceApplication {
 
     public static void main(String[] args) {
